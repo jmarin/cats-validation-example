d6239350c14620e03d737fbadee258561b67c903
Juan Marin Otero <juan.marin.otero@gmail.com>
2022-10-15 01:11:19 -0400

    Fold results for better print out

--- shared/src/main/scala/com/jmarin/examples/validation/Main.scala
+++ shared/src/main/scala/com/jmarin/examples/validation/Main.scala
@@ -15,75 +15,89 @@ object ValidationMain:
     val now = LocalDateTime.now().format(formatter).toString()
 
     // Validation using Either?
-    val registeredEitherUser = EitherValidator.validateRegistration(
-      "jmarinotero",
-      "MySuper$_SecretP@assword",
-      "Juan",
-      "Marin",
-      25,
-      "juan@email.com",
-      "(111)-349-5555",
-      "I live in this address",
-      "20007",
-      "Washington",
-      "DC",
-      now
-    )
-    println(
-      s"Either Validation, valid user:\n ${registeredEitherUser.map(_.asJson)}"
-    )
+    val registeredEitherUser = EitherValidator
+      .validateRegistration(
+        "jmarinotero",
+        "MySuper$_SecretP@assword",
+        "Juan",
+        "Marin",
+        25,
+        "juan@email.com",
+        "(111)-349-5555",
+        "I live in this address",
+        "20007",
+        "Washington",
+        "DC",
+        now
+      )
+      .fold(
+        e => s"Either Validation, Invalid user: $e.toString()}",
+        a => a.asJson
+      )
 
-    val invalidEitherUser = EitherValidator.validateRegistration(
-      "jmarinotero",
-      "password",
-      "Juan",
-      "Marin",
-      25,
-      "juanemail.com",
-      "(111)-349-5555",
-      "I live in this address",
-      "207",
-      "Washington",
-      "Europa",
-      now
-    )
-    println(
-      s"Either Validation, invalid user:\n ${invalidEitherUser.map(_.asJson)}"
-    )
+    println(registeredEitherUser)
+
+    val invalidEitherUser = EitherValidator
+      .validateRegistration(
+        "jmarinotero",
+        "password",
+        "Juan",
+        "Marin",
+        25,
+        "juanemail.com",
+        "(111)-349-5555",
+        "I live in this address",
+        "207",
+        "Washington",
+        "Europa",
+        now
+      )
+      .fold(
+        e => s"Either Validation, Invalid user: $e.toString()}",
+        a => a.asJson
+      )
+    println(invalidEitherUser)
 
     // A better approach, using ValidatedNel
-    val registeredValidatedUser = CatsValidation.validateRegistration(
-      "jmarinotero",
-      "MySuper$_SecretP@assword",
-      "Juan",
-      "Marin",
-      25,
-      "juan@email.com",
-      "(111)-349-5555",
-      "I live in this address",
-      "20007",
-      "Washington",
-      "DC",
-      now
-    )
-    println(
-      s"ValidatedNel Validation, valid user: ${registeredValidatedUser.map(_.asJson)}"
-    )
+    val registeredValidatedUser = CatsValidation
+      .validateRegistration(
+        "jmarinotero",
+        "MySuper$_SecretP@assword",
+        "Juan",
+        "Marin",
+        25,
+        "juan@email.com",
+        "(111)-349-5555",
+        "I live in this address",
+        "20007",
+        "Washington",
+        "DC",
+        now
+      )
+      .fold(
+        e => s"Cats Validation, Invalid user: $e.toString()}",
+        a => a.asJson
+      )
 
-    val invalidValidateduser = CatsValidation.validateRegistration(
-      "jmarinotero",
-      "password",
-      "Juan",
-      "Marin",
-      25,
-      "juanemail.com",
-      "(111)-349-5555",
-      "I live in this address",
-      "207",
-      "Washington",
-      "Europa",
-      now
-    )
-    println(
-      s"ValidatedNel validation, invalid user:\n ${invalidValidateduser.map(_.asJson)}"
-    )
+    println(registeredValidatedUser)
+
+    val invalidValidateduser = CatsValidation
+      .validateRegistration(
+        "jmarinotero",
+        "password",
+        "Juan",
+        "Marin",
+        25,
+        "juanemail.com",
+        "(111)-349-5555",
+        "I live in this address",
+        "207",
+        "Washington",
+        "Europa",
+        now
+      )
+      .fold(
+        e => s"Cats Validation, Invalid user: $e.toString()}",
+        a => a.asJson
+      )
+    println(invalidValidateduser)
